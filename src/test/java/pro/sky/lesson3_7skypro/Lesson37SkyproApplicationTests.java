584e3554e2d3afa5b0d844ac9ed6ee82f6293dbd
MakTub <7andreyru@mail.ru>
2023-03-03 23:11:40 +0500

    ДЗ к уроку 3_7

---
+++ src/test/java/pro/sky/lesson3_7skypro/Lesson37SkyproApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson3_7skypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson37SkyproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
